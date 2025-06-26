50eb21bf7d876f8ae331ada2f0517551d0e03b0f
Abhi <avvjit@gmail.com>
2025-06-26 12:02:22 +0530

    get string response

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express')
+// import express from('express')  --> alternative mordan call approch
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/khushi', (req, res)=> {
+res.send('i love you baby')
+})
+
+app.get(
+    '/null', (req, res)=> {
+        res.send('its me abhi')
+    }
+)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
